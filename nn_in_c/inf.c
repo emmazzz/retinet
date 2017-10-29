3621c6aecf2ae7e9386d2e139f02dfa8e0828022
Jun-hong-Li <lijunhongjack@live.cn>
2017-10-28 22:21:28 -0400

    synth'd, resources exceeded

--- nn_in_c/inf.c
+++ nn_in_c/inf.c
@@ -2,15 +2,25 @@
 #include <stdio.h>
 #include <math.h>
 
-double input[724];
-double hidOut[20];
+double inf_input[784];
+double hidOut[70];
+double tmp[72];
+double probs[10];
 
+#define CAPSTONE_HLS
 
 int main() {
 
+	int i, j;
 	// write into input
+#ifdef CAPSTONE_HLS
+	for (i = 0; i < 28*28; i++) {
+#pragma HLS UNROLL
+		inf_input[i] = i / 256.0;
+	}
+#else
 	FILE *f = fopen("processed","r");
-	for (int i = 0; i < 28*28; ++i)
+	for (i = 0; i < 28*28; ++i)
 	{
 		int tmp, bla;
 		fread(&tmp, 4, 1, f);
@@ -18,13 +28,14 @@ int main() {
 		double temp = (double) tmp;
 		printf("%f\n", temp);
 		input[i] = temp/256.0;
-		
 	}
+#endif
 
-	for (int i = 0;i < 70;i++) {
+	for (i = 0;i < 70;i++) {
 		double sum = hiddenWeights[i][0];
 		for (int j = 1;j < 785;j++) {
-			sum += hiddenWeights[i][j]*input[j-1];
+#pragma HLS UNROLL
+			sum += hiddenWeights[i][j]*inf_input[j-1];
 		}
 		// activate
 		hidOut[i] = 1.0/(1.0 + exp(-1.0*sum));
@@ -33,19 +44,27 @@ int main() {
 	double maxProb = 0;
 	int res = 0;
 
-	for (int i = 0;i < 10;i++) {
+	for (i = 0;i < 10;i++) {
+#pragma HLS UNROLL
 		double sum = outputWeights[i][0];
-		for (int j = 1;j < 71;j++) {
-			sum += outputWeights[i][j]*hidOut[j-1];
+		for (j = 1;j < 71;j++) {
+#pragma HLS UNROLL
+			tmp[i] = outputWeights[i][j]*hidOut[j-1];
+		}
+		for (j = 1; j < 71; j++) {
+			sum += tmp[i];
 		}
 		// activate
 		sum = 1.0/(1.0 + exp(-1.0*sum));
-		printf("%d: prob = %f\n", i, sum);
-		if (sum > maxProb) {
-			maxProb = sum;
+//		printf("%d: prob = %f\n", i, sum);
+		probs[i] = sum;
+	}
+
+	for (i = 0; i < 10; i++) {
+		if (probs[i] > maxProb) {
+			maxProb = probs[i];
 			res = i;
 		}
-
 	}
 
 	// printf("result is %d\n" ,res);
